14fbe7a25fad089fb681e9b537c8963d1ca11bcf
akiwa <fwgedei@yahoo.de>
2021-02-17 18:55:18 +0100

    First crude version of web service

--- app/main.py
+++ app/main.py
@@ -1,37 +1,73 @@
-from flask import Flask, request
+from pathlib import Path
+from typing import List
+
+from flask import Flask, request, render_template, redirect, url_for, flash
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
 from redis import Redis
 from rq import Queue
+from uuid import uuid4
 
+from config import set_flask_configs
 from image_to_mesh import image_to_mesh
 
-q = Queue(connection=Redis())
+task_queue = Queue(connection=Redis())
 
 app = Flask(__name__)
+set_flask_configs(app)
 
 
-@app.route("/queue")
-def queue() -> str:
-    i = request.args.get("input")
-    try:
-        n = int(i)
-        job = q.enqueue(image_to_mesh.do_work, n)
-        return f"Enqueued job with id {job.id}"
-    except ValueError:
-        return "Input must be a number"
+def allowed_extension(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("base.html")
+
+
+@app.route("/", methods=["POST"])
+def file_upload():
+    uploaded_files: List[FileStorage] = []
+
+    file: FileStorage
+    for file in [request.files.get(f) for f in request.files]:
+        if allowed_extension(file.filename):
+            uploaded_files.append(file)
+        else:
+            return "Invalid file extension", 400
+
+    folder: Path = app.config["UPLOAD_FOLDER"] / str(uuid4())
+    if not folder.exists():
+        folder.mkdir(parents=True)
+
+    processed_filenames: List[str] = []
+    for file in uploaded_files:
+        processed_filename: str = str(folder / secure_filename(file.filename))
+        processed_filenames.append(processed_filename)
+        file.save(processed_filename)
+
+    job = task_queue.enqueue(image_to_mesh.process_images, processed_filenames)
+    return job.id
+
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
 
 
 @app.route("/result")
 def get_result():
     i = request.args.get("id")
-    job = q.fetch_job(i)
+    job = task_queue.fetch_job(i)
 
     if job is None:
         return f"No job found with id {i}"
     elif job.result is None:
         return f"Job with id {i} not finished yet"
     else:
-        return f"Job with id {i} finished with {job.result}"
+        return f"Job with id {i} finished with:<br />{job.result}"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=1234)
+    app.run(host="0.0.0.0", port=12345)
--- image_to_mesh/image_to_mesh.py
+++ image_to_mesh/image_to_mesh.py
@@ -1,7 +1,22 @@
 import time
+from pathlib import Path
+from typing import List
+from os import remove, rmdir
 
 
-def do_work(n: int) -> int:
-    print(f"Staring work with {n}")
-    time.sleep(10)
-    return n + 1
+def process_images(image_list: List[str]) -> str:
+    print(f"Starting work with images {str(image_list)}")
+    time.sleep(5)
+
+    file: Path
+    for file in [Path(file) for file in image_list]:
+        if file.exists() and file.is_file():
+            remove(file)
+        time.sleep(2)
+
+    time.sleep(2)
+    folder = Path(image_list[0]).parent
+    if folder.exists() and folder.is_dir() and not any(folder.iterdir()):
+        rmdir(folder)
+
+    return "Finished processing pipeline"
