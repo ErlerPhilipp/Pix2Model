0452abe5c49b457e2ed899f1f9cc565c24257e80
sophie <sophie.pichler@ufficina.de>
2021-08-09 12:28:05 +0200

    Disable upload

--- web/src/upload/UploadComponent.jsx
+++ web/src/upload/UploadComponent.jsx
@@ -8,21 +8,39 @@ function Upload(props) {
   const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
   
   // called every time a file's `status` changes
-  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+  const handleChangeStatus = ({ meta, remove }, status) => {
+    if (status === 'headers_received') {
+      console.log(meta)
+      //document.querySelector("#description").innerHTML = `Upload successful!\nJob created with id:<br/><a href="/result?id=${response}">${response}</a>`;
+      document.querySelector("#description").innerHTML = `Upload successful!\nJob created with id:<br/>`;
+      //remove()
+    } else if (status === 'aborted') {
+      console.log(meta)
+      document.querySelector("#description").innerHTML = `Upload failed`;
+    }
+  }
+
   
   // receives array of files that are done uploading when submit button is clicked
-  const handleSubmit = (files) => { console.log(files.map(f => f.meta)) }
+  const handleSubmit = (files, allFiles) => { 
+    allFiles.forEach(f => f.remove());
+  }
+
+  const handleSuccess = (file, response) => {
+  }
 
   return (
     <div class='upload'>
-        <p class='description'>
+        <p class='description' id='description'>
         Upload between 10-50 images to create your mesh
         </p>
     <Dropzone
       getUploadParams={getUploadParams}
+      inputContent='Drop files here to upload. Max 100 files, 10MB per file'
       onChangeStatus={handleChangeStatus}
       onSubmit={handleSubmit}
-      accept="image/*,audio/*,video/*"
+      onSuccess={handleSuccess}
+      accept="image/*"
     />
     </div>
   )
