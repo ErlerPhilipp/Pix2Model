9ec6ddab66fed4d46907f6dd0bc03708ad443b00
sophie-sudoku <sophie.pichler@ufficina.de>
2021-09-23 21:13:02 +0200

    Add boundingbox with measures

--- web/src/edit/EditComponent.js
+++ web/src/edit/EditComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import * as THREE from "three";
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 import { PLYExporter } from 'three/examples/jsm/exporters/PLYExporter.js';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls';
@@ -18,16 +19,23 @@ class Edit extends Component {
   }
 
   componentDidMount() {
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var camera = new THREE.PerspectiveCamera(75, (window.innerWidth - 300) / (window.innerHeight - 75), 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth - 300, window.innerHeight - 75);
     this.scene.background = new THREE.Color(0xfbebc3);
     const light = new THREE.AmbientLight(0x404040); // soft white light
     this.scene.add(light);
     this.mount.appendChild(renderer.domElement);
+
+    var labelRenderer = new CSS2DRenderer();
+    labelRenderer.setSize(window.innerWidth - 300, window.innerHeight - 75);
+    labelRenderer.domElement.style.position = 'absolute';
+    labelRenderer.domElement.style.top = '0px';
+    this.mount.appendChild(labelRenderer.domElement);
+
     camera.position.z = 5;
-    const controls = new OrbitControls(camera, renderer.domElement);
-    this.transformControls = new TransformControls(camera, renderer.domElement);
+    const controls = new OrbitControls(camera, labelRenderer.domElement);
+    this.transformControls = new TransformControls(camera, labelRenderer.domElement);
     this.loader = new Loader(this);
     var scope = this;
     this.transformControls.addEventListener('mouseDown', function () {
@@ -54,12 +62,15 @@ class Edit extends Component {
       }
     })
     window.addEventListener('resize', function() {
+      camera.aspect = (window.innerWidth - 300) / (window.innerHeight - 75);
+      camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth - 300, window.innerHeight - 75);
     })
     // animation
     var animate = function () {
       requestAnimationFrame(animate);
       renderer.render(scope.scene, camera);
+      labelRenderer.render( scope.scene, camera );
       controls.update();
     };
     animate();
@@ -79,6 +90,9 @@ class Edit extends Component {
     }, false);
     this.buildFileSelector();
     this.createCropBox();
+    this.createLabels();
+    this.box3 = new THREE.Box3();
+    this.size = new THREE.Vector3();
   }
 
   buildFileSelector(){
@@ -103,7 +117,6 @@ class Edit extends Component {
     } else {
       this.object = object;
     }
-    console.log(this.object)
     this.scene.add(this.object);
     this.transformControls.attach(this.object);
     this.setState({loaded: true, name: filename});
@@ -127,6 +140,10 @@ class Edit extends Component {
     var scale = {x: this.object.scale.x, y: this.object.scale.y, z: this.object.scale.z};
     var translation = {x: this.object.position.x, y: this.object.position.y, z: this.object.position.z};
     this.setState({rotation, scale, translation});
+    if (this.boxHelper) {
+      this.boxHelper.update();
+      this.updateLabels();
+    }
   }
 
   handleDownload() {
@@ -154,7 +171,6 @@ class Edit extends Component {
         new THREE.BufferAttribute(new Float32Array(normals), 3)
       );
     }
-    console.log(this.object)
     const bspObject = CSG.fromMesh(this.object);
     const bspBox = CSG.fromMesh(this.cropBox);                        
     const bspResult = bspBox.inverse().intersect(bspObject.inverse());
@@ -192,6 +208,9 @@ class Edit extends Component {
     this.transformControls.detach(this.object);
     this.scene.remove(this.object);
     this.scene.remove(this.cropBox);
+    if (this.boxHelper) {
+      this.scene.remove(this.boxHelper);
+    }
     this.setState({loaded: false, name: ''});
   }
 
@@ -206,6 +225,57 @@ class Edit extends Component {
       this.scene.remove(this.cropBox);
       this.transformControls.attach(this.object);
     }
+  }
+
+  activateMeasurement(measure) {
+    if (measure) {
+      this.boxHelper = new THREE.BoxHelper( this.object, 0x37474F );
+      this.scene.add( this.boxHelper );
+      this.object.add( this.xLabel);
+      this.object.add(this.yLabel);
+      this.object.add(this.zLabel);
+      this.updateLabels();
+    } else if (this.boxHelper) {
+      this.scene.remove(this.boxHelper);
+      this.object.remove( this.xLabel);
+      this.object.remove(this.yLabel);
+      this.object.remove(this.zLabel);
+    }
+  }
+
+  createLabels() {
+    this.xDiv = document.createElement( 'div' );
+    this.xDiv.className = 'label x';
+    this.xDiv.style.marginTop = '-1em';
+    this.xLabel = new CSS2DObject( this.xDiv );
+    
+    this.yDiv = document.createElement( 'div' );
+    this.yDiv.className = 'label y';
+    this.yDiv.style.marginBottom = '-1em';
+    this.yLabel = new CSS2DObject(this.yDiv);
+
+    this.zDiv = document.createElement( 'div' );
+    this.zDiv.className = 'label z';
+    this.zDiv.style.marginTop = '-1em';
+    this.zLabel = new CSS2DObject( this.zDiv );
+  }
+
+  updateLabels() {
+    if (!this.boxHelper) {
+      return;
+    }
+    this.object.geometry.computeBoundingBox();
+    this.box3.copy( this.object.geometry.boundingBox ).applyMatrix4( this.object.matrixWorld );
+    this.box3.getSize(this.size);
+
+    this.xDiv.textContent = this.size.x;
+    this.xLabel.position.set(this.size.x / 2, 0, 0,);
+
+    this.yDiv.textContent = this.size.y;
+    this.yLabel.position.set(0, this.size.y / 2, 0);
+
+    this.zDiv.textContent = this.size.z;
+    this.zLabel.position.set(0, 0, this.size.z / 2);
   }
 
   render() {
@@ -241,6 +311,14 @@ class Edit extends Component {
               {!this.state.crop &&
                 <button class="crop_button" disabled>Crop</button>
               }
+              <hr></hr>
+              <p class="heading_interaction">MEASUREMENT</p>
+              <label class="container">Activate
+                <input id="measure" type="checkbox" onClick={(event) => {
+                  this.activateMeasurement(event.target.checked);
+                }}/>
+                <span class="checkmark"></span>
+              </label>
               <button onClick={this._handleDownload} class='edit_download'><i class="fa fa-download"></i></button>
               <button onClick={this._handleRemove} class='edit_remove'><i class="fa fa-remove"></i></button>
             </div>
