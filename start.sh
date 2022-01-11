2edca76cdbbdb2f0ad633862a72e018ffd7bb626
Florian Steinschorn <f.steinschorn@gmail.com>
2022-01-11 10:54:06 +0100

    remove start.sh output

--- start.sh
+++ start.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-set -x
 
 # remove old docker images and containers
-docker stop $(docker ps -a -q --filter ancestor=images2mesh_client)
-docker stop $(docker ps -a -q --filter ancestor=images2mesh_worker)
-docker stop $(docker ps -a -q --filter ancestor=images2mesh_backend)
-docker stop $(docker ps -a -q --filter ancestor=images2mesh_dashboard)
-docker rm $(docker ps -a -q --filter ancestor=images2mesh_client)
-docker rm $(docker ps -a -q --filter ancestor=images2mesh_worker)
-docker rm $(docker ps -a -q --filter ancestor=images2mesh_backend)
-docker rm $(docker ps -a -q --filter ancestor=images2mesh_dashboard)
-docker rmi -f $(docker images -q --filter=reference=images2mesh_client)
-docker rmi -f $(docker images -q --filter=reference=images2mesh_worker)
-docker rmi -f $(docker images -q --filter=reference=images2mesh_backend)
-docker rmi -f $(docker images -q --filter=reference=images2mesh_dashboard)
-docker rmi $(docker images -f dangling=true -q)
+echo "Stopping containers ..."
+docker stop $(docker ps -a -q --filter ancestor=images2mesh_client) &>/dev/null
+docker stop $(docker ps -a -q --filter ancestor=images2mesh_worker) &>/dev/null
+docker stop $(docker ps -a -q --filter ancestor=images2mesh_backend) &>/dev/null
+docker stop $(docker ps -a -q --filter ancestor=images2mesh_dashboard) &>/dev/null
+echo "Removing containers ..."
+docker rm $(docker ps -a -q --filter ancestor=images2mesh_client) &>/dev/null
+docker rm $(docker ps -a -q --filter ancestor=images2mesh_worker) &>/dev/null
+docker rm $(docker ps -a -q --filter ancestor=images2mesh_backend) &>/dev/null
+docker rm $(docker ps -a -q --filter ancestor=images2mesh_dashboard) &>/dev/null
+echo "Removing images ..."
+docker rmi -f $(docker images -q --filter=reference=images2mesh_client) &>/dev/null
+docker rmi -f $(docker images -q --filter=reference=images2mesh_worker) &>/dev/null
+docker rmi -f $(docker images -q --filter=reference=images2mesh_backend) &>/dev/null
+docker rmi -f $(docker images -q --filter=reference=images2mesh_dashboard) &>/dev/null
+echo "Removing dangling images ..."
+docker rmi $(docker images -f dangling=true -q) &>/dev/null
 
 # build new images and start
+echo "Starting Images2Mesh ..."
 docker-compose --profile develop up
