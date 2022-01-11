715a3e4cac259c1068f433cc965a2213967eb4bd
sophie <sophie.pichler@ufficina.de>
2022-01-12 00:23:17 +0100

    Refine UI for image upload

--- web/src/upload/UploadComponent.jsx
+++ web/src/upload/UploadComponent.jsx
@@ -10,48 +10,57 @@ class Upload extends Component {
   constructor(props){
     super(props);
     Dropzone.autoDiscover = false;
-    const that = this;
-    if( document.readyState == 'complete' ) {
-      setTimeout(function(){ that.init(); }, 3000)
-    } else {
-      document.addEventListener("DOMContentLoaded", () => {
-        this.init();
-      });
-    }
+    this.state = {
+      success: false
+    };
   }
 
   init() {
-    const submitButton = document.querySelector("#submit_upload");
-    submitButton.addEventListener("click", eventArgs => {
-        document.querySelector("#upload").dropzone.processQueue();
-    });
-    document.querySelector("#upload").dropzone.on("sending", function(file, xhr, data) {
-        if (data) {
-          data.append("user_email", document.querySelector("#email").value);
-          data.append("step2", + !document.querySelector("#ply").checked);
-        }
-    });
-    document.querySelector("#upload").dropzone.on("success", (file, response) => {
-        console.log('succesful...')
-        submitButton.disablwrapper_uploaded = true;
-        document.querySelector("#response_field").innerHTML = `Upload successful!\nJob created with id:<br/><a href="/backend/result?id=${response}">${response}</a>`;
-    });
+    const that = this
+    setTimeout(function() {
+      var dropzoneOptions = {
+        paramName: "file",
+        autoProcessQueue: false,
+        uploadMultiple: true,
+        parallelUploads: 100,
+        maxFilesize: 50,
+        maxFiles: 100,
+        acceptedFiles: ".png, .jpg, .jpeg",
+        dictDefaultMessage: "Drop files here to upload<br/><small>Max 100 files, 10MB per file</small>"
+      };
+      var uploader = document.querySelector('#upload');
+      that.dropzone = new Dropzone(uploader, dropzoneOptions);
+      const submitButton = document.querySelector("#submit_upload");
+      submitButton.addEventListener("click", eventArgs => {
+          document.querySelector("#upload").dropzone.processQueue();
+      });
+      document.querySelector("#upload").dropzone.on("sending", function(file, xhr, data) {
+          if (data) {
+            data.append("user_email", document.querySelector("#email").value);
+            data.append("step2", + !document.querySelector("#ply").checked);
+          }
+      });
+      document.querySelector("#upload").dropzone.on("success", (file, response) => {
+          that.setState({success: true})
+          submitButton.disablwrapper_uploaded = true;
+          document.querySelector("#response_field").innerHTML = `Upload successful!\nJob created with id:<br/><a href="/backend/result?id=${response}">${response}</a>`;
+      });
+    }, 0);
   }
 
   componentDidMount() {
-    const { t } = this.props;
-    var dropzoneOptions = {
-      paramName: "file",
-      autoProcessQueue: false,
-      uploadMultiple: true,
-      parallelUploads: 100,
-      maxFilesize: 50,
-      maxFiles: 100,
-      acceptedFiles: ".png, .jpg, .jpeg",
-      dictDefaultMessage: "Drop files here to upload<br/><small>Max 100 files, 10MB per file</small>"
-    };
-    var uploader = document.querySelector('#upload');
-    var newDropzone = new Dropzone(uploader, dropzoneOptions);
+    if (!this.state.success) {
+      this.init();
+    }
+  }
+
+  reset() {
+    this.setState({success: false})
+    this.init()
+  }
+
+  resetDropbox() {
+    this.dropzone.removeAllFiles(true); 
   }
 
   render() {
@@ -59,30 +68,47 @@ class Upload extends Component {
     return (
       <div class='content'>
         <div class='wrapper_centered_box'>
-          <img src='images3.png' class='images'/>
-          <img src='mesh.png' class='mesh'/>
-          <i class="arrow first"></i>
-          <i class="arrow second"></i>
-          <i class="arrow third"></i>
-          <i class="arrow fourth"></i>
-          <form class="dropzone" method="POST" action="/backend/" id="upload">
-          </form>
-          <small class="hint">{t('upload.files.support')} JPG, JPEG, PNG</small><br />
-          <button id="submit_upload" class="button_small">{t('upload.submit')}</button><br />
-          <small id="response_field">
-          </small>
-          <hr></hr>
-          <div class="formfield">
-          <input type="checkbox" id="ply" name="ply"></input>
-          <label for="ply" class="formfield">{t('upload.pc.label')}</label>
-          <small class="hint"><br></br>{t('upload.pc.hint')}</small>
+        {!this.state.success &&
+          <div>
+            <img src='images3.png' class='images'/>
+            <img src='mesh.png' class='mesh'/>
+            <i class="arrow first"></i>
+            <i class="arrow second"></i>
+            <i class="arrow third"></i>
+            <i class="arrow fourth"></i>
+            <form class="dropzone" method="POST" action="/backend/" id="upload">
+            </form>
+            <small class="hint">{t('upload.files.support')} JPG, JPEG, PNG</small><br />
+            <div class="edit_buttons">
+              <button id="submit_upload" class="button_small">{t('upload.submit')}</button>
+              <button onClick={() => {this.resetDropbox()}} class="button_small">{t('upload.reset')}</button>
+            </div>
+            <br />
+            <hr></hr>
+            <div class="formfield">
+            <input type="checkbox" id="ply" name="ply"></input>
+            <label for="ply" class="formfield">{t('upload.pc.label')}</label>
+            <small class="hint"><br></br>{t('upload.pc.hint')}</small>
+            </div>
+            <hr></hr>
+            <div class="formfield">
+            <label for="email" class="formfield">Email <big><sup>*</sup></big></label>
+            <input type="email" id="email" name="email" class="formfield_input"></input>
+            <small class="hint"><br></br>{t('upload.email')}<br></br></small>
+            </div>
           </div>
-          <hr></hr>
-          <div class="formfield">
-          <label for="email" class="formfield">Email <big><sup>*</sup></big></label>
-          <input type="email" id="email" name="email" class="formfield_input"></input>
-          <small class="hint"><br></br>{t('upload.email')}<br></br></small>
+        }
+        {this.state.success &&
+          <div>
+            <h2>SUCCESS
+            </h2>
+            <hr></hr>
+            <p id="response_field">
+            </p>
+            <hr></hr>
+            <button onClick={() => {this.reset()}} class="button_small">{t('upload.new')}</button><br />
           </div>
+        }
         </div>
       </div>
     )
