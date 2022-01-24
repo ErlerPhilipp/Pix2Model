ee31044467238c015e1f1ac0d9bd1f09681d7389
sophie <sophie.pichler@ufficina.de>
2022-01-24 20:50:05 +0100

    Add tooltips and change settings depending which model is loaded

--- web/src/CustomTabComponent.js
+++ web/src/CustomTabComponent.js
@@ -1,7 +1,6 @@
 import SwipeableViews from 'react-swipeable-views';
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import HomeIcon from '@material-ui/icons/Home';
 
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
