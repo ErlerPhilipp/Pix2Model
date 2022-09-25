69dd8a3397ee5737b658e63192228bea796b0fa0
sophie <sophie.pichler@ufficina.de>
2022-09-25 22:16:41 +0200

    Add font imports

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,13 @@ import React, {Suspense} from 'react'
 import ReactDOM from 'react-dom';
 
 import './fonts/Roboto-Regular.ttf';
+import './fonts/Roboto-Bold.ttf';
+import './fonts/Roboto-Italic.ttf';
+
+import './fonts/Roboto-Regular.eot';
+import './fonts/Roboto-Bold.eot';
+import './fonts/Roboto-Italic.eot';
+
 import './index.css';
 import './react-tabs-custom.css';
 import './i18n';
