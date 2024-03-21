8b16f692bbb487faf81cdffc76a53a8da2808e9b
ertz10 <eliasbrugger03@gmail.com>
2024-03-21 09:04:28 +0100

    changed image2mesh tag in create_base_image.sh

--- create_base_image.sh
+++ create_base_image.sh
@@ -4,4 +4,4 @@ set -x
 
 docker rmi -f $(docker images -q --filter=reference=images2mesh_base)
 docker rmi $(docker images -f dangling=true -q)
-docker build -t images2mesh/images2mesh:base -f Dockerfile.base .
+docker build -t image2mesh/image2mesh:base -f Dockerfile.base .
