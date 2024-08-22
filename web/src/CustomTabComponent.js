37370f0044dd59cc223b92c63c8d1c7b67a2e5f3
joschi1212 <e1427350@student.tuwien.ac.at>
2024-08-22 16:44:16 +0200

    add icons and links to about, impressum, info and privacy page, make menu close after click

--- web/src/CustomTabComponent.js
+++ web/src/CustomTabComponent.js
@@ -24,9 +24,19 @@ function CustomTabs() {
   const [state, setState] = useState('upload')
   const [index, setIndex] = useState(new URLSearchParams(window.location.search).get('page') ? new URLSearchParams(window.location.search).get('page') : 0);
   const [load_id, setLoadId] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleSetState = (newState) => {
     setState(newState);
+    closeMenu();
+  };
+
+  const handleStateChange = (state) => {
+    setMenuOpen(state.isOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
 
   function handleChangeLang(lang) {
@@ -50,72 +60,92 @@ function CustomTabs() {
   }
 
   return (
-      <React.StrictMode>
-        <BrowserView>
-          <div className="Root">
+    <React.StrictMode>
+      <BrowserView>
+        <div className="Root">
           <Tabs
-              value={index}
-              fullwidth="true"
-              onChange={handleChange}
-              TabIndicatorProps={{style: {backgroundColor: "white"}}}>
-              <Tab label="Pix-2-Model (Beta)"/>
-              <Tab label={t('menu.edit')}/>
-              <Tab icon={t('menu.about')}/>
+            value={index}
+            fullwidth="true"
+            onChange={handleChange}
+            TabIndicatorProps={{style: {backgroundColor: "white"}}}>
+            <Tab label="Pix-2-Model (Beta)"/>
+            <Tab label={t('menu.edit')}/>
+            <Tab icon={t('menu.about')}/>
           </Tabs>
           <button className='lang en' onClick={() => handleChangeLang('en')}>EN</button>
           <button className='lang de' onClick={() => handleChangeLang('de')}>DE</button>
           <SwipeableViews index={index} disabled={true} onChangeIndex={handleChangeIndex} enableMouseEvents={true}>
-              <div className='swipable_content'>
-                  <Upload></Upload>
-              </div>
-              <div className='swipable_content'>
-                  <Edit></Edit>
-              </div>
-              <div className='swipable_content'>
-                  <About></About>
-              </div>
-              <div className='swipable_content'>
-                  <Support></Support>
-              </div>
-              <div className='swipable_content'>
-                  <Privacy></Privacy>
-              </div>
-              <div className='swipable_content'>
-                  <Impressum></Impressum>
-              </div>
+            <div className='swipable_content'>
+              <Upload></Upload>
+            </div>
+            <div className='swipable_content'>
+              <Edit></Edit>
+            </div>
+            <div className='swipable_content'>
+              <About></About>
+            </div>
+            <div className='swipable_content'>
+              <Support></Support>
+            </div>
+            <div className='swipable_content'>
+              <Privacy></Privacy>
+            </div>
+            <div className='swipable_content'>
+              <Impressum></Impressum>
+            </div>
           </SwipeableViews>
           <footer className="footer">
-              <button onClick={() => setIndex(5)}>{t('footer.impressum')}</button>
-              <button onClick={() => setIndex(4)}>{t('footer.privacy')}</button>
-              <button onClick={() => setIndex(3)}>{t('footer.support')}</button>
+            <button onClick={() => setIndex(5)}>{t('footer.impressum')}</button>
+            <button onClick={() => setIndex(4)}>{t('footer.privacy')}</button>
+            <button onClick={() => setIndex(3)}>{t('footer.support')}</button>
           </footer>
-          </div>
-        </BrowserView>
-        <MobileView>
-          <div className='mobile_wrapper'>
-            <Menu>
-              <SideMenuItem title="Upload" image="images.png" stateName="upload" state={state} setState={handleSetState} ></SideMenuItem>
-              <SideMenuItem title="Pointcloud" image="ply.png" stateName="pointcloud" state={state} setState={handleSetState} ></SideMenuItem>
-              <SideMenuItem title="Mesh" image="mesh.png" stateName="mesh" state={state} setState={handleSetState} ></SideMenuItem>
-              <div>
-                <input id="uuid"></input>
-                <button className="button_small" onClick={() => { onLoadIdPressed() }}>{t('upload.loadPointcloud')}</button>
-              </div>
-              <button id="uuid_error" className="edit_warning"></button>
-            </Menu>
-              {state === 'upload' && (
-                <Upload></Upload>
-              )}
-              {state === 'pointcloud' && (
-                <Edit load_id={load_id} step="pointcloud" ></Edit>
-              )}
-              {state === 'mesh' && (
-                <Edit load_id={load_id} step="mesh" ></Edit>
-              )}
-          </div>
-        </MobileView>
-      </React.StrictMode>
-    );
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className='mobile_wrapper'>
+          <Menu 
+          isOpen={menuOpen}
+          onStateChange={(state) => handleStateChange(state)}>
+            <SideMenuItem title="Upload" image="images.png" stateName="upload" state={state} setState={handleSetState} ></SideMenuItem>
+            <SideMenuItem title="Pointcloud" image="ply.png" stateName="pointcloud" state={state} setState={handleSetState} ></SideMenuItem>
+            <SideMenuItem title="Mesh" image="mesh.png" stateName="mesh" state={state} setState={handleSetState} ></SideMenuItem>
+            <div className="id_container">
+              <input id="uuid"></input>
+              <button className="button_small" onClick={() => { onLoadIdPressed(); closeMenu(); }}>{t('upload.loadPointcloud')}</button>
+            </div>
+            <button id="uuid_error" className="edit_warning"></button>
+            <div className="icon_container">
+              <a href="#" onClick={(e) => { e.preventDefault(); handleSetState("support"); closeMenu();}}><img alt="info" src="icon_info.svg"/></a>
+              <a href="#" onClick={(e) => { e.preventDefault(); handleSetState("impressum"); closeMenu();}}><img alt="impressum" src="icon_impressum.svg"/></a>
+              <a href="#" onClick={(e) => { e.preventDefault(); handleSetState("privacy"); closeMenu();}}><img alt="privacy" src="icon_privacy.svg"/></a>
+              <a href="#" onClick={(e) => { e.preventDefault(); handleSetState("about"); closeMenu();}}><img alt="about" src="icon_about.svg"/></a>
+            </div>
+          </Menu>
+          {state === 'upload' && (
+            <Upload></Upload>
+          )}
+          {state === 'pointcloud' && (
+            <Edit load_id={load_id} step="pointcloud" ></Edit>
+          )}
+          {state === 'mesh' && (
+            <Edit load_id={load_id} step="mesh" ></Edit>
+          )}
+          {state === 'support' && (
+            <Support></Support>
+          )}
+          {state === 'impressum' && (
+            <Impressum></Impressum>
+          )}
+          {state === 'privacy' && (
+            <Privacy></Privacy>
+          )}
+          {state === 'about' && (
+            <About></About>
+          )}
+        </div>
+      </MobileView>
+    </React.StrictMode>
+  );
   }
   
   export default CustomTabs;
